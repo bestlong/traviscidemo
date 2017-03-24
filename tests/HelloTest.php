1dd7923e81cff21225160d558af2b55e54666442
Shao Yu Lung <bestlong168@gmail.com>
2017-03-25 00:25:28 +0800

    fix error

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -5,6 +5,6 @@ class HelloTest extends PHPUnit_Framework_TestCase
     public function testOne()
     {
         $stack = [1];
-        $this->assertEquals(0, count($stack));
+        $this->assertEquals(1, count($stack));
     } 
 }
